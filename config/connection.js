8e4192c283e3d1c04b0327273b9ff90ed79890af
Aditya Fitriadi <aditya.fitriadi99@gmail.com>
2023-11-18 10:05:05 +0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
-    host: process.env.DATABASE_HOST,
+const sequelize = new Sequelize(process.env.POSTGRE_URL, {
+    // host: process.env.DATABASE_HOST,
     dialect: "postgres",
     logging: false
 })
