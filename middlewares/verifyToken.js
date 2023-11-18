6381d7172697149eb1d99646be45682f20f36431
Aditya Fitriadi <aditya.fitriadi99@gmail.com>
2023-11-18 10:22:06 +0700

    Update verifyToken.js

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -3,10 +3,18 @@ const jwt = require('jsonwebtoken')
 
 async function verifyToken(req, res, next) {
     const tokenHeader = req.headers.authorization
+
+    if(tokenHeader === undefined) {
+        return res.status(400).send({
+            status: "error",
+            message: "must provide token"
+        })
+    }
+
     const token = tokenHeader.split(' ')
 
     if (token[0] !== 'Bearer') {
-        return res.status(500).send({
+        return res.status(401).send({
             status: "error",
             message: "incorrect token format"
         })
