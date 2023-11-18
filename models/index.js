8f52ede55b830f668d9efc5c684e8e7302f07fb7
Aditya Fitriadi <aditya.fitriadi99@gmail.com>
2023-11-18 15:11:56 +0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,20 +1,18 @@
 'use strict';
 
+require('dotenv').config()
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-let sequelize
-if (config['url']) {
-  sequelize = new Sequelize(config['url'], config);
-} else {
-  sequelize = new Sequelize(config['database'], config['username'], config['password'], config);
-}
+const sequelize = new Sequelize(process.env.POSTGRE_URL, {
+  dialect: "postgres",
+  logging: false
+})
 
 fs
   .readdirSync(__dirname)
